7860fb1fb9524042689e5ec9963cdeaee11930f0
seppe <r0888765@student.thomasmore.be>
2022-09-23 11:39:58 +0200

    bundle 2 1.5

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import {PRODUCTS_DATA} from "./data/data";
 import {MenuCardPage} from "./pages/MenuCardPage";
 import {PicturesPage} from "./pages/PicturesPage";
+import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
+import 'react-tabs/style/react-tabs.css';
 
 function App() {
   return (
       <>
-        <MenuCardPage product={PRODUCTS_DATA}/>
-          <PicturesPage/>
+    <Tabs defaultIndex={0} onSelect={(index) => console.log(index)}>
+        <TabList>
+            <Tab>Menu</Tab>
+            <Tab>Pictures</Tab>
+        </TabList>
+        <TabPanel><MenuCardPage product={PRODUCTS_DATA}/></TabPanel>
+        <TabPanel><PicturesPage/></TabPanel>
+    </Tabs>
       </>
   );
 }
