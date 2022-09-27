f4162afbcccff138fc43c2d742fc8aebd83b273f
seppe <r0888765@student.thomasmore.be>
2022-09-27 16:15:51 +0200

    bundle 3 3.3

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,29 +1,50 @@
 import {COLOR_DATA} from "../data/data";
+import {array} from "prop-types";
 
-export function Cars(props){
+export function Cars(props) {
     const {cars} = props;
     const {title} = props;
-    if(!cars) return;
+    if (!cars) return;
     return <>
         <h3 style={{textAlign: "center", fontWeight: "bold"}}>
             {title}
         </h3>
-        <div style={{display:"flex", height:"100px", width:"100%" , flexWrap:"wrap", alignItems:"center" ,marginBottom:"150px"}}>
-            {cars.map(c =>
-                <div key={c.id} style={{border:"solid 1px",  backgroundColor:"#ffffff", width:" 100%", margin:"10px", textAlign:"center"}} >
-                    <h5>{c.name? c.name : ""}</h5>
-                    <div>{c.brand? `merk: ${c.brand}` : ""}</div>
-                    <div>{c.type? `type: ${c.type}` : ""}</div>
-                    <div>{c.note? `opmerking: ${c.note}` : ""}</div>
-                    <div> {c.color? <div> kleur: {c.color} </div> : ""} </div>
-                </div>)}
+        <div style={{
+            display: "flex",
+            height: "100px",
+            width: "100%",
+            flexWrap: "wrap",
+            alignItems: "center",
+            marginBottom: "150px"
+        }}>
+            {cars.map(c => <Car key={c.id} car={c}/>)}
         </div>
     </>
 }
-//
-// export function Translate(props) {
-//     const {colors} = props
-//     const {color} = props
-//     const EL = document.getElementsByClassName("background-color")
-//     EL.style.backgroundColor=`${ color? colors.find(color).map.english: "" }`;
-// }
+
+export function Car(props) {
+    const {car} = props
+    return <>
+        <div key={car.id} style={{
+            border: "solid 1px",
+            backgroundColor: "#ffffff",
+            width: " 100%",
+            margin: "10px",
+            textAlign: "center"}}>
+
+            <h5>{car.name ? car.name : ""}</h5>
+            <div>{car.brand ? `merk: ${car.brand}` : ""}</div>
+            <div>{car.type ? `type: ${car.type}` : ""}</div>
+            <div>{car.note ? `opmerking: ${car.note}` : ""}</div>
+            <div>
+                {car.color ? <div style={car?.color && {backgroundColor: Translate(COLOR_DATA, car.color)}}> kleur: {car.color} </div> : ""}
+            </div>
+        </div>
+    </>
+
+
+}
+
+export function Translate(array, color) {
+    return (array.find(el => el.dutch === color).english)
+}
