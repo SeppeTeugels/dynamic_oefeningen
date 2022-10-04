3308e6554b7bda8947fefe871f22693f724d853f
seppe <r0888765@student.thomasmore.be>
2022-10-04 10:22:01 +0200

    bundel 4 2.7

--- src/pages/EventsAndStatePage.js
+++ src/pages/EventsAndStatePage.js
@@ -3,11 +3,10 @@ import {EventDemo} from "../components/EventDemo";
 export function EventsAndStatePage() {
     return(
         <>
-            <div style={{backgroundColor:"lavender", textAlign:"center"}} className={"mx-3"}>
-                <h1>Events</h1>
+            <div style={{ backgroundColor:"lavender"}} className={"mx-3"}>
+                <h1 style={{textAlign:"center"}}>Events</h1>
                 <EventDemo/>
             </div>
         </>
-
     )
 }
