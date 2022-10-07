d51a00158e696dfeeeafb66160438408e03a66ab
seppe <r0888765@student.thomasmore.be>
2022-10-07 10:43:07 +0200

    bundel 4 4.4

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,18 @@
 import {Button, Container, Row} from "react-bootstrap";
+import {useState} from "react";
 
 export function Section(props) {
+    const [isOpen, setIsOpen] = useState(false)
     const {title, children} = props;
     return (<>
         <div  style={{backgroundColor: "#e6e6fa", marginBottom:"20px"}}>
             <h3 style={{textAlign: "center", fontWeight: "bold"}}>
                 {title}
-                <Button variant={"primary"} style={{margin: "5px"}} onClick={() => alert("klik klik klik")}>klik</Button>
+                <Button variant={"primary"} style={{margin: "5px"}} onClick={() => setIsOpen(!isOpen)}>{(isOpen)?"sluit":"open"}</Button>
             </h3>
             <Container>
                 <Row>
-                    {children}
+                    {(isOpen)?children:""}
                 </Row>
             </Container>
         </div>
